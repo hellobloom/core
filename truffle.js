1362da15c0a81aac54c73cbdd0ac89a4eb05a519
John Backus <johncbackus@gmail.com>
2017-10-22 18:20:47 -0700

    Add in-memory test network

--- truffle.js
+++ truffle.js
@@ -4,6 +4,10 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    test: {
+      provider: require("ethereumjs-testrpc").provider({ gasLimit: 1e7 }),
+      network_id: "*"
     }
   }
 };
