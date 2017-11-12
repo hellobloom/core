8a203255372326ec0c26232b8ba2d8e37b10a459
John Backus <johncbackus@gmail.com>
2017-11-11 20:49:59 -0800

    Use truffle test network for tests
    Not having the test issues I had before, it is faster to boot and run,
    and it requires less config

--- truffle.js
+++ truffle.js
@@ -4,10 +4,6 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    test: {
-      provider: require("ethereumjs-testrpc").provider({ gasLimit: 1e7 }),
-      network_id: "*"
     }
   }
 };
