a4d71dd4fb4056d9797a8234171ed4f901bf80e5
djvs <djvansch@gmail.com>
2018-10-04 13:06:41 -0400

    Use deterministic names for anonymous methods instead

--- generate_method_manifest.ts
+++ generate_method_manifest.ts
@@ -97,13 +97,9 @@ function buildMember(member: Member): string {
 
 function buildFunctionMember(member: FunctionMember) {
   //console.log("MEMBER", member)
-  const inputsWithNames = member.inputs.map((x: any) => {
+  const inputsWithNames = member.inputs.map((x: any, i: number) => {
     if (!x.name) {
-      x.name =
-        "anonymous_" +
-        Math.random()
-          .toString()
-          .substr(2, 14)
+      x.name = "anonymous_" + i.toString()
     }
     return x
   })
@@ -230,6 +226,8 @@ glob("./build/contracts/*.json", {}, (err: string, files: string[]) => {
 
     method_buffer += buildContract(definition, isTruffle)
   })
+
+  // buffer += `export type TContractNames = keyof typeof EContractNames\n\n`
 
   buffer += `export type TContractNames = "` + contract_names.join(`" | "`) + `"\n\n`
 
