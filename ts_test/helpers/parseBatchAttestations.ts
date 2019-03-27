4a177e92b54da50d9e3eaa0879246cc44cd55f7c
Isaac Patka <ipatka@gmail.com>
2019-03-26 23:14:43 -0400

    all contracts updated to 5.0.7

--- ts_test/helpers/parseBatchAttestations.ts
+++ ts_test/helpers/parseBatchAttestations.ts
@@ -1,4 +1,4 @@
-const fs = require("fs")
+const fs = require('fs')
 interface IAttestation {
   requester: string
   attester: string
@@ -7,11 +7,19 @@ interface IAttestation {
 }
 export const parseBatchAttestations = async (filePath: string) => {
   const fileContents = fs.readFileSync(filePath)
-  const lines = fileContents.toString().split("\n")
+  const lines = fileContents.toString().split('\n')
   const firstLine = lines[0]
   const dataLines = lines.slice(1, lines.length - 1)
   const dataParsed: IAttestation[] = dataLines.map((line: string) =>
-    firstLine.split(",").reduce((curr: any, next: any, index: any) => ({ ...curr, [next]: line.split(",")[index] }), {})
+    firstLine
+      .split(',')
+      .reduce(
+        (curr: any, next: any, index: any) => ({
+          ...curr,
+          [next]: line.split(',')[index],
+        }),
+        {}
+      )
   )
   const dataFormatted = {
     requesters: dataParsed.map(a => a.requester),
