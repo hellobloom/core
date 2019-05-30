70dc4080c7fa4df14c3d2291d14c8d44e84ca8e3
Isaac Patka <ipatka@gmail.com>
2019-05-30 17:06:17 -0400

    bump to 0.5.9

--- contracts/AccreditationRepo.sol
+++ contracts/AccreditationRepo.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.7;
+pragma solidity 0.5.9;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
--- contracts/AttestationLogic.sol
+++ contracts/AttestationLogic.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.7;
+pragma solidity 0.5.9;
 
 import "./SigningLogic.sol";
 import "./TokenEscrowMarketplace.sol";
